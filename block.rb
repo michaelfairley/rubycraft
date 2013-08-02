8d51b55b0a3b242e25ff2d4619f1a99d93d84b4f
Michael Fairley <michaelfairley@gmail.com>
2013-08-01 19:36:59 -0500

    A faster thing

--- block.rb
+++ block.rb
@@ -26,6 +26,19 @@ class Block
   BRB = [P, N, P]
   BRT = [P, P, P]
 
+  TOP1    = FLT+FLB+BLB
+  TOP2    = BLB+BLT+FLT
+  BOTTOM1 = FLB+FRB+BRB
+  BOTTOM2 = BRB+BLB+FLB
+  FRONT1  = FRT+FRB+FLB
+  FRONT2  = FLB+FLT+FRT
+  BACK1   = BLT+BLB+BRB
+  BACK2   = BRB+BRT+BLT
+  RIGHT1  = BRT+BRB+FRB
+  RIGHT2  = FRB+FRT+BRT
+  LEFT1   = FLT+FLB+BLB
+  LEFT2   = BLB+BLT+FLT
+
   module TEX_COORDS
     TOP    = [[0.0/16, 0.0/16], [0.0/16, 1.0/16], [1.0/16, 1.0/16], [1.0/16, 0.0/16]]
     SIDE   = [[3.0/16, 0.0/16], [3.0/16, 1.0/16], [4.0/16, 1.0/16], [4.0/16, 0.0/16]]
@@ -46,6 +59,44 @@ class Block
     @faces
   end
 
+  def flb; [x1, y1, z1]; end
+  def flt; [x1, y2, z1]; end
+  def frb; [x2, y1, z1]; end
+  def frt; [x2, y2, z1]; end
+  def blb; [x1, y1, z2]; end
+  def blt; [x1, y2, z2]; end
+  def brb; [x2, y1, z2]; end
+  def brt; [x2, y2, z2]; end
+
+  def top    ; frt+flt+blt+brt; end
+  def front  ; flb+flt+frt+frb; end
+  def back   ; brb+brt+blt+blb; end
+  def bottom ; flb+frb+brb+blb; end
+  def right  ; frb+frt+brt+brb; end
+  def left   ; blb+blt+flt+flb; end
+
+  def tex_top    ; [0, 1, 0, 0, 1, 0, 1, 1]; end
+  def tex_side   ; [3, 1, 3, 0, 4, 0, 4, 1]; end
+  def tex_bottom ; [2, 1, 2, 0, 3, 0, 3, 1]; end
+
+  def top_color   ; [0.3, 0.6, 0.4, 1]; end
+  def other_color ; [1,   1,   1,   1]; end
+
+  def vertices
+    @vertices ||= top + bottom + front + back + right + left
+  end
+
+  def tex_coords
+    @tex_coords ||=
+      (
+      tex_top + tex_bottom + tex_side + tex_side + tex_side + tex_side
+      ).map{ |c| c/16.0 }
+  end
+
+  def colors
+    @colors ||= top_color*4 + other_color*5*4
+  end
+
   def draw
     self.class.compile_lists
 
@@ -61,150 +112,150 @@ class Block
     "#<Block: {@loc}>"
   end
 
-  def self.compile_top_list
-    LISTS[:top] = next_list_id
+  # def self.compile_top_list
+  #   LISTS[:top] = next_list_id
 
-    glNewList(LISTS[:top], GL_COMPILE)
+  #   glNewList(LISTS[:top], GL_COMPILE)
 
-    glBegin(GL_QUADS)
-      glColor4f(0.3, 0.6, 0.4, 1)
-      glTexCoord2f(*TEX_COORDS::TOP[0])
-      glVertex3f(*FRT)
-      glTexCoord2f(*TEX_COORDS::TOP[1])
-      glVertex3f(*FLT)
-      glTexCoord2f(*TEX_COORDS::TOP[2])
-      glVertex3f(*BLT)
-      glTexCoord2f(*TEX_COORDS::TOP[3])
-      glVertex3f(*BRT)
-    glEnd
+  #   glBegin(GL_QUADS)
+  #     glColor4f(0.3, 0.6, 0.4, 1)
+  #     glTexCoord2f(*TEX_COORDS::TOP[0])
+  #     glVertex3f(*FRT)
+  #     glTexCoord2f(*TEX_COORDS::TOP[1])
+  #     glVertex3f(*FLT)
+  #     glTexCoord2f(*TEX_COORDS::TOP[2])
+  #     glVertex3f(*BLT)
+  #     glTexCoord2f(*TEX_COORDS::TOP[3])
+  #     glVertex3f(*BRT)
+  #   glEnd
 
-    glEndList
-  end
+  #   glEndList
+  # end
 
-  def self.compile_front_list
-    LISTS[:front] = next_list_id
+  # def self.compile_front_list
+  #   LISTS[:front] = next_list_id
 
-    glNewList(LISTS[:front], GL_COMPILE)
+  #   glNewList(LISTS[:front], GL_COMPILE)
 
-    glBegin(GL_QUADS)
-      glColor4f(1, 1, 1, 1)
-      glTexCoord2f(*TEX_COORDS::SIDE[0])
-      glVertex3f(*FRT)
-      glTexCoord2f(*TEX_COORDS::SIDE[1])
-      glVertex3f(*FRB)
-      glTexCoord2f(*TEX_COORDS::SIDE[2])
-      glVertex3f(*FLB)
-      glTexCoord2f(*TEX_COORDS::SIDE[3])
-      glVertex3f(*FLT)
-    glEnd
+  #   glBegin(GL_QUADS)
+  #     glColor4f(1, 1, 1, 1)
+  #     glTexCoord2f(*TEX_COORDS::SIDE[0])
+  #     glVertex3f(*FRT)
+  #     glTexCoord2f(*TEX_COORDS::SIDE[1])
+  #     glVertex3f(*FRB)
+  #     glTexCoord2f(*TEX_COORDS::SIDE[2])
+  #     glVertex3f(*FLB)
+  #     glTexCoord2f(*TEX_COORDS::SIDE[3])
+  #     glVertex3f(*FLT)
+  #   glEnd
 
-    glEndList
-  end
+  #   glEndList
+  # end
 
-  def self.compile_right_list
-    LISTS[:right] = next_list_id
+  # def self.compile_right_list
+  #   LISTS[:right] = next_list_id
 
-    glNewList(LISTS[:right], GL_COMPILE)
+  #   glNewList(LISTS[:right], GL_COMPILE)
 
-    glBegin(GL_QUADS)
-      glColor4f(1, 1, 1, 1)
-      glTexCoord2f(*TEX_COORDS::SIDE[0])
-      glVertex3f(*BRT)
-      glTexCoord2f(*TEX_COORDS::SIDE[1])
-      glVertex3f(*BRB)
-      glTexCoord2f(*TEX_COORDS::SIDE[2])
-      glVertex3f(*FRB)
-      glTexCoord2f(*TEX_COORDS::SIDE[3])
-      glVertex3f(*FRT)
-    glEnd
+  #   glBegin(GL_QUADS)
+  #     glColor4f(1, 1, 1, 1)
+  #     glTexCoord2f(*TEX_COORDS::SIDE[0])
+  #     glVertex3f(*BRT)
+  #     glTexCoord2f(*TEX_COORDS::SIDE[1])
+  #     glVertex3f(*BRB)
+  #     glTexCoord2f(*TEX_COORDS::SIDE[2])
+  #     glVertex3f(*FRB)
+  #     glTexCoord2f(*TEX_COORDS::SIDE[3])
+  #     glVertex3f(*FRT)
+  #   glEnd
 
-    glEndList
-  end
+  #   glEndList
+  # end
 
-  def self.compile_left_list
-    LISTS[:left] = next_list_id
+  # def self.compile_left_list
+  #   LISTS[:left] = next_list_id
 
-    glNewList(LISTS[:left], GL_COMPILE)
+  #   glNewList(LISTS[:left], GL_COMPILE)
 
-    glBegin(GL_QUADS)
-      glColor4f(1, 1, 1, 1)
-      glTexCoord2f(*TEX_COORDS::SIDE[0])
-      glVertex3f(*FLT)
-      glTexCoord2f(*TEX_COORDS::SIDE[1])
-      glVertex3f(*FLB)
-      glTexCoord2f(*TEX_COORDS::SIDE[2])
-      glVertex3f(*BLB)
-      glTexCoord2f(*TEX_COORDS::SIDE[3])
-      glVertex3f(*BLT)
-    glEnd
+  #   glBegin(GL_QUADS)
+  #     glColor4f(1, 1, 1, 1)
+  #     glTexCoord2f(*TEX_COORDS::SIDE[0])
+  #     glVertex3f(*FLT)
+  #     glTexCoord2f(*TEX_COORDS::SIDE[1])
+  #     glVertex3f(*FLB)
+  #     glTexCoord2f(*TEX_COORDS::SIDE[2])
+  #     glVertex3f(*BLB)
+  #     glTexCoord2f(*TEX_COORDS::SIDE[3])
+  #     glVertex3f(*BLT)
+  #   glEnd
 
-    glEndList
-  end
+  #   glEndList
+  # end
 
-  def self.compile_back_list
-    LISTS[:back] = next_list_id
+  # def self.compile_back_list
+  #   LISTS[:back] = next_list_id
 
-    glNewList(LISTS[:back], GL_COMPILE)
+  #   glNewList(LISTS[:back], GL_COMPILE)
 
-    glBegin(GL_QUADS)
-      glColor4f(1, 1, 1, 1)
-      glTexCoord2f(*TEX_COORDS::SIDE[0])
-      glVertex3f(*BLT)
-      glTexCoord2f(*TEX_COORDS::SIDE[1])
-      glVertex3f(*BLB)
-      glTexCoord2f(*TEX_COORDS::SIDE[2])
-      glVertex3f(*BRB)
-      glTexCoord2f(*TEX_COORDS::SIDE[3])
-      glVertex3f(*BRT)
-    glEnd
+  #   glBegin(GL_QUADS)
+  #     glColor4f(1, 1, 1, 1)
+  #     glTexCoord2f(*TEX_COORDS::SIDE[0])
+  #     glVertex3f(*BLT)
+  #     glTexCoord2f(*TEX_COORDS::SIDE[1])
+  #     glVertex3f(*BLB)
+  #     glTexCoord2f(*TEX_COORDS::SIDE[2])
+  #     glVertex3f(*BRB)
+  #     glTexCoord2f(*TEX_COORDS::SIDE[3])
+  #     glVertex3f(*BRT)
+  #   glEnd
 
-    glEndList
-  end
+  #   glEndList
+  # end
 
-  def self.compile_bottom_list
-    LISTS[:bottom] = next_list_id
+  # def self.compile_bottom_list
+  #   LISTS[:bottom] = next_list_id
 
-    glNewList(LISTS[:bottom], GL_COMPILE)
+  #   glNewList(LISTS[:bottom], GL_COMPILE)
 
-    glBegin(GL_QUADS)
-      glColor4f(1, 1, 1, 1)
-      glTexCoord2f(*TEX_COORDS::BOTTOM[0])
-      glVertex3f(*FLB)
-      glTexCoord2f(*TEX_COORDS::BOTTOM[1])
-      glVertex3f(*FRB)
-      glTexCoord2f(*TEX_COORDS::BOTTOM[2])
-      glVertex3f(*BRB)
-      glTexCoord2f(*TEX_COORDS::BOTTOM[3])
-      glVertex3f(*BLB)
-    glEnd
+  #   glBegin(GL_QUADS)
+  #     glColor4f(1, 1, 1, 1)
+  #     glTexCoord2f(*TEX_COORDS::BOTTOM[0])
+  #     glVertex3f(*FLB)
+  #     glTexCoord2f(*TEX_COORDS::BOTTOM[1])
+  #     glVertex3f(*FRB)
+  #     glTexCoord2f(*TEX_COORDS::BOTTOM[2])
+  #     glVertex3f(*BRB)
+  #     glTexCoord2f(*TEX_COORDS::BOTTOM[3])
+  #     glVertex3f(*BLB)
+  #   glEnd
 
-    glEndList
-  end
+  #   glEndList
+  # end
 
-  def self.compile_lists
-    return  unless dirty?
-    @@list_counter = glGenLists(6)
-    compile_top_list
-    compile_front_list
-    compile_left_list
-    compile_right_list
-    compile_back_list
-    compile_bottom_list
-    @dirty = false
-  end
+  # def self.compile_lists
+  #   return  unless dirty?
+  #   @@list_counter = glGenLists(6)
+  #   compile_top_list
+  #   compile_front_list
+  #   compile_left_list
+  #   compile_right_list
+  #   compile_back_list
+  #   compile_bottom_list
+  #   @dirty = false
+  # end
 
-  def self.dirty!
-    @dirty = true
-  end
+  # def self.dirty!
+  #   @dirty = true
+  # end
 
-  def self.dirty?
-    @dirty
-  end
+  # def self.dirty?
+  #   @dirty
+  # end
 
-  def self.next_list_id
-    @@list_counter +=1
-    @@list_counter - 1
-  end
+  # def self.next_list_id
+  #   @@list_counter +=1
+  #   @@list_counter - 1
+  # end
 
-  self.dirty!
+  # self.dirty!
 end
--- rubycraft.rb
+++ rubycraft.rb
@@ -4,6 +4,7 @@ require 'gosu'
 require 'hasu'
 require 'opengl'
 require 'ruby-prof'
+require 'perftools'
 
 Hasu.load 'block.rb'
 Hasu.load 'player.rb'
@@ -24,7 +25,7 @@ class Rubycraft < Gosu::Window
     @texture = @image.gl_tex_info
     glBindTexture(GL_TEXTURE_2D, @texture.tex_name)
     glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST)
-    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST)
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR)
   end
 
   def reset
@@ -53,8 +54,16 @@ class Rubycraft < Gosu::Window
     build_block(1, 0, -5)
     build_block(0, 0, -6)
     build_block(1, 0, -6)
+    build_block(5, 0, -6)
+
+    build_block(2, 5, -3)
 
     @player = Player.new(@blocks)
+
+    if @buffers
+      glDeleteBuffers(@buffers)
+      @buffers = nil
+    end
   end
 
   def build_block(x, y, z)
@@ -86,7 +95,33 @@ class Rubycraft < Gosu::Window
     self.mouse_y = HEIGHT/2
   end
 
+  def fill_data
+    @buffers = glGenBuffers(3)
+    @vert_vbo, @tex_vbo, @color_vbo = @buffers
+
+    vertices = @blocks.values.flat_map(&:vertices)
+    vert_data = vertices.pack('f*')
+    glBindBuffer(GL_ARRAY_BUFFER, @vert_vbo)
+    glBufferData(GL_ARRAY_BUFFER, vertices.size*4, vert_data, GL_STATIC_DRAW)
+
+    tex_coords = @blocks.values.flat_map(&:tex_coords)
+    tex_data = tex_coords.pack('f*')
+    glBindBuffer(GL_ARRAY_BUFFER, @tex_vbo)
+    glBufferData(GL_ARRAY_BUFFER, tex_coords.size*4, tex_data, GL_STATIC_DRAW)
+
+    colors = @blocks.values.flat_map(&:colors)
+    color_data = colors.pack('f*')
+    glBindBuffer(GL_ARRAY_BUFFER, @color_vbo)
+    glBufferData(GL_ARRAY_BUFFER, colors.size*4, color_data, GL_STATIC_DRAW)
+
+    raise  unless vertices.size/3 == tex_coords.size/2
+    raise  unless vertices.size/3 == colors.size/4
+    @vertices_count = vertices.size/3
+  end
+
   def draw
+    fill_data  if @buffers.nil?
+
     gl do
       glEnable(GL_TEXTURE_2D)
       glBlendFunc(GL_SRC_ALPHA,GL_ONE)
@@ -98,6 +133,7 @@ class Rubycraft < Gosu::Window
       glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST)
       glEnable(GL_CULL_FACE)
       glCullFace(GL_BACK)
+      # glDepthRange(0, 0)
 
       glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
       glBindTexture(GL_TEXTURE_2D, @texture.tex_name)
@@ -115,15 +151,30 @@ class Rubycraft < Gosu::Window
       glLightfv(GL_LIGHT1, GL_AMBIENT, ambient_light)
       # glLightfv(GL_LIGHT1, GL_DIFFUSE, diffuse_light)
       # glLightfv(GL_LIGHT1, GL_POSITION, light_postion)
-      glEnable(GL_LIGHT1)
-      glEnable(GL_LIGHTING)
+      # glEnable(GL_LIGHT1)
+      # glEnable(GL_LIGHTING)
       glEnable(GL_COLOR_MATERIAL)
 
       glRotatef(@player.y_angle, 0, 1, 0)
       glRotatef(-@player.x_angle, Math.cos(@player.y_angle * Math::PI / 180), 0, Math.sin(@player.y_angle * Math::PI / 180))
       glTranslate(-@player.x, -@player.y, -@player.z)
 
-      @blocks.values.each(&:draw)
+      glColor4f(1, 1, 1, 1)
+
+      glBindBuffer(GL_ARRAY_BUFFER, @vert_vbo)
+      glVertexPointer(3, GL_FLOAT, 0, 0)
+
+      glBindBuffer(GL_ARRAY_BUFFER, @tex_vbo)
+      glTexCoordPointer(2, GL_FLOAT, 0, 0)
+
+      glBindBuffer(GL_ARRAY_BUFFER, @color_vbo)
+      glColorPointer(4, GL_FLOAT, 0, 0)
+
+      glEnableClientState(GL_VERTEX_ARRAY)
+      glEnableClientState(GL_TEXTURE_COORD_ARRAY)
+      glEnableClientState(GL_COLOR_ARRAY)
+
+      glDrawArrays(GL_QUADS, 0, @vertices_count)
     end
 
     if @state == :paused
@@ -147,7 +198,9 @@ class Rubycraft < Gosu::Window
 
         printer = RubyProf::FlatPrinter.new(result)
         printer.print(STDOUT)
+        # PerfTools::CpuProfiler.stop
       else
+        # PerfTools::CpuProfiler.start("/tmp/rubycraft_profile")
         RubyProf.start
         @profiling = true
         puts "Profiling..."
