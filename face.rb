605eb12b791ca9c3dc6505567a8cb33a31a3529f
Michael Fairley <michaelfairley@gmail.com>
2013-08-04 12:53:11 -0500

    Better texture builder

--- face.rb
+++ face.rb
@@ -5,6 +5,14 @@ class Face
     @vertices = vertices.freeze
   end
 
+  def self.texture(x, y)
+    [x+0, y+1,
+     x+0, y+0,
+     x+1, y+0,
+     x+1, y+1
+    ].map{ |c| c/16.0 }.freeze
+  end
+
   COLORS = ([1, 1, 1, 1] * 4).freeze
   def colors
     COLORS
@@ -17,29 +25,28 @@ class GrassFace < Face
     COLORS
   end
 
-  TEX_COORDS = [0, 1, 0, 0, 1, 0, 1, 1].map{ |c| c/16.0 }.freeze
+  TEX_COORDS = texture(0, 0)
   def tex_coords
     TEX_COORDS
   end
 end
 
 class DirtFace < Face
-  TEX_COORDS = [2, 1, 2, 0, 3, 0, 3, 1].map{ |c| c/16.0 }.freeze
+  TEX_COORDS = texture(2, 0)
   def tex_coords
     TEX_COORDS
   end
 end
 
 class DirtSideFace < Face
-  TEX_COORDS = [3, 1, 3, 0, 4, 0, 4, 1].map{ |c| c/16.0 }.freeze
+  TEX_COORDS = texture(3, 0)
   def tex_coords
     TEX_COORDS
   end
 end
 
-
 class StoneFace < Face
-  TEX_COORDS = [1, 1, 1, 0, 2, 0, 2, 1].map{ |c| c/16.0 }.freeze
+  TEX_COORDS = texture(1, 0)
   def tex_coords
     TEX_COORDS
   end
