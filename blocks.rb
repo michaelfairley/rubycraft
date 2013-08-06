d15119c17a1b3a6a4670456f6161d40f97a6417f
Michael Fairley <michaelfairley@gmail.com>
2013-08-05 23:25:30 -0500

    Chunks can always be made lazily

--- blocks.rb
+++ blocks.rb
@@ -9,31 +9,23 @@ module Blocks
     x = loc.x.to_i / Chunk::SIZE * Chunk::SIZE
     z = loc.z.to_i / Chunk::SIZE * Chunk::SIZE
 
-    _chunks[[x, z]]
-  end
-
-  def self.ensure_chunk_for_point(loc)
-    x = loc.x.to_i / Chunk::SIZE * Chunk::SIZE
-    z = loc.z.to_i / Chunk::SIZE * Chunk::SIZE
-
     _chunks.fetch([x, z]) do |x, z|
       _chunks[[x, z]] = Chunk.new(x, z)
     end
   end
 
   def self.exists?(loc)
-    chunk = ensure_chunk_for_point(loc)
-    chunk && chunk.exists?(loc)
+    _chunk_for_point(loc).exists?(loc)
   end
 
   def self._dirty_neighbors!(loc)
     loc.sides.map do |loc|
       _chunk_for_point(loc)
-    end.compact.each(&:dirty!)
+    end.each(&:dirty!)
   end
 
   def self.add!(block)
-    ensure_chunk_for_point(block.loc).add!(block)
+    _chunk_for_point(block.loc).add!(block)
     _dirty_neighbors!(block.loc)
   end
 
@@ -56,7 +48,7 @@ module Blocks
     (-Player::SIGHT..Player::SIGHT).step(Chunk::SIZE).flat_map do |x|
       depth = Math.sqrt(Player::SIGHT**2 - x ** 2)
       (-depth..depth).step(Chunk::SIZE).map do |z|
-        ensure_chunk_for_point(Point.new(x, 0, z) + player.loc)
+        _chunk_for_point(Point.new(x, 0, z) + player.loc)
       end
     end
   end
