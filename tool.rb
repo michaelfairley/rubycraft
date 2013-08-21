bae51f3cbeda6100e4e1ea747f41847d67ea7d60
Michael Fairley <michaelfairley@gmail.com>
2013-08-21 18:49:01 -0500

    Texture the hand

--- tool.rb
+++ tool.rb
@@ -14,25 +14,22 @@ class Hand
   def draw
     glPushMatrix
 
-    glDisable(GL_TEXTURE_2D)
-
     glTranslate(0.2, -0.25 + Math.sin(@bob)*0.02 + Math.sin(@swing/Math::PI)*0.05, -0.5)
 
     glRotatef(-20 + Math.sin(@bob) * 3, 0, 0, 1)
     glRotatef(-60 + Math.sin(@bob) * 5 + Math.sin(@swing/Math::PI) * 10, 1, 0, 0)
-    glRotatef(Math.sin(@swing/3.0) * 40, 0, 0, 1)
+    glRotatef(Math.sin(@swing/3.0) * 30, 0, 0, 1)
 
+    tex_coords = [[0, 0], [0, 0.25/16.0], [1/16.0, 0.25/16.0], [1/16.0, 0]] * 2
+    colors = [[255, 223, 196, 255].map{|v| v/256.0}] * 8
 
     glBegin(GL_QUADS) do
-      glColor4f(0.5, 0, 0, 1)
-
-      vertices.each_slice(3) do |x, y, z|
+      vertices.each_slice(3).zip(tex_coords, colors).each do |(x, y, z), (t, u), (r, g, b, a)|
+        glColor4f(r, g, b, a)
+        glTexCoord2f(t, u)
         glVertex3f(x, y, z)
       end
     end
-
-    glEnable(GL_TEXTURE_2D)
-
 
     glPopMatrix
 
@@ -51,10 +48,10 @@ class Hand
       WIDTH, LENGTH, 0,
       0, LENGTH, 0,
       # left
-      0, -LENGTH, 0,
       0, LENGTH, 0,
       0, LENGTH, -WIDTH,
       0, -LENGTH, -WIDTH,
+      0, -LENGTH, 0,
     ]
   end
 end
